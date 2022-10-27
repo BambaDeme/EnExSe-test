ec7449df55748245555b1ed23d8b76fa32b0662b
bambademe <cheikhbambademe@gmail.com>
2022-10-27 12:06:35 +0000

    init project

---
+++ src/main/java/com/demecheikhahmadoubamba/EnExSetest/EnExSeTestApplication.java
@@ -0,0 +1,13 @@
+package com.demecheikhahmadoubamba.EnExSetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnExSeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnExSeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demecheikhahmadoubamba/EnExSetest/EnExSeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demecheikhahmadoubamba.EnExSetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnExSeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
