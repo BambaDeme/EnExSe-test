7a3e994a11cb71f345d4602f5615142e45c7273b
bambademe <cheikhbambademe@gmail.com>
2022-10-27 12:29:40 +0000

    repository

---
+++ src/main/java/com/demecheikhahmadoubamba/EnExSetest/repository/AgentRepository.java
@@ -0,0 +1,9 @@
+package com.demecheikhahmadoubamba.EnExSetest.repository;
+
+import com.demecheikhahmadoubamba.EnExSetest.entity.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgentRepository extends JpaRepository<Agent,Long> {
+}
